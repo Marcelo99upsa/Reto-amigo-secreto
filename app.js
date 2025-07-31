cb6df2d1c3d492c7f0018ac9b261df65133788a8
Marcelo99upsa <a2018119133@estudiantes.upsa.edu.bo>
2025-07-31 17:16:15 -0400

    Desafio Final

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let listaHTML = document.getElementById("listaAmigos");
+
+
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById("amigo").value;
+    let li = document.createElement("li");
+
+    if (nombreDeAmigo == ""){
+        alert("Por favor, inserte un nombre.");
+    }
+    else
+        if (amigos.map(nombre => nombre.toLowerCase()).includes(nombreDeAmigo.toLowerCase())) {
+        alert("Este nombre ya esta repetido.");
+        }
+        else{
+            amigos.push(nombreDeAmigo);
+            li.textContent = nombreDeAmigo;
+            listaHTML.appendChild(li);
+        }
+    borrarCampo();
+    console.log(amigos);
+}
+
+function borrarCampo() {
+    let inputAmigo = document.getElementById("amigo");
+    inputAmigo.value = "";
+    inputAmigo.focus();
+}
+
+function sortearAmigo() {
+    let amigoSorteado = document.getElementById("resultado");
+    let maximo = amigos.length;
+    let indiceAleatorio = Math.floor(Math.random() * maximo);
+    if (amigos.length == 0) {
+        alert("La lista de amigos esta vacia.");
+    }
+    else{
+        console.log(amigos[indiceAleatorio]);
+        amigoSorteado.innerText = amigos[indiceAleatorio];
+    }
+}
